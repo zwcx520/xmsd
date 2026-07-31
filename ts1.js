c1e53829cef58dac8aad3e1f3e929005f63ad224
追吻辰星 <1837601513@qq.com>
2026-08-01 07:20:11 +0800

    Update ts1.js

--- ts1.js
+++ ts1.js
@@ -260,7 +260,6 @@ if (savedVersion !== String(PLAY_VERSION)) {
 
 
 
-/***
 
 // 锁定页面所有触控、鼠标操作
 function lockPageTouch() {
@@ -319,4 +318,3 @@ function unlockPageTouch() {
 lockPageTouch();  // 锁定
 // unlockPageTouch(); // 解锁
 
-***/
